63a8dff312cd35e46d0d2b75e872f35495632746
william_trouvaille <william453945663@gmail.com>
2023-04-25 22:44:22 +0800

    -

--- src/main/java/com/trouvaille/aladdin/controller/ShoppingCartController.java
+++ src/main/java/com/trouvaille/aladdin/controller/ShoppingCartController.java
@@ -1,6 +1,8 @@
 package com.trouvaille.aladdin.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.trouvaille.aladdin.common.BaseContext;
 import com.trouvaille.aladdin.common.R;
 import com.trouvaille.aladdin.entity.ShoppingCart;
 import com.trouvaille.aladdin.service.ShoppingCartService;
@@ -8,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -29,6 +32,75 @@ public class ShoppingCartController {
     private ShoppingCartService shoppingCartService;
 
 
+    /**
+     * @param :
+     * @return R<List < ShoppingCart>>
+     * @author willi
+     * @description 查看购物车
+     * @date 2023/04/24 20:04
+     */
+    @GetMapping("/list")
+    public R<List<ShoppingCart>> list() {
+        log.info("查看购物车");
+        Long userid = BaseContext.getCurrentId();
+
+        LambdaQueryWrapper<ShoppingCart> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(ShoppingCart::getUserId, userid);
+        lqw.orderByAsc(ShoppingCart::getCreateTime);
+
+        List<ShoppingCart> shoppingCarts = shoppingCartService.list(lqw);
+
+
+        return R.success(shoppingCarts);
+    }
+
+    /**
+     * @param :
+     * @return R<String>
+     * @author willi
+     * @description 清空购物车
+     * @date 2023/04/24 20:04
+     */
+    @DeleteMapping("/clean")
+    public R<String> clean() {
+
+        LambdaQueryWrapper<ShoppingCart> lqw = new LambdaQueryWrapper<>();
+        Long userid = BaseContext.getCurrentId();
+
+        lqw.eq(ShoppingCart::getUserId, userid);
+        boolean flag = shoppingCartService.remove(lqw);
+
+        return R.flag(flag);
+    }
+
+    @PostMapping("/add")
+    public R<ShoppingCart> add(@RequestBody ShoppingCart shoppingCart) {
+        log.info("添加购物车信息==.{}", shoppingCart.toString());
+
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+
+        LambdaQueryWrapper<ShoppingCart> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(ShoppingCart::getUserId, shoppingCart.getUserId());
+        lqw.eq(ShoppingCart::getCommodityId, shoppingCart.getCommodityId());
+
+        ShoppingCart cartServiceOne = shoppingCartService.getOne(lqw);
+
+        if (cartServiceOne != null) {
+            Integer number = cartServiceOne.getNumber();
+            cartServiceOne.setNumber(number + 1);
+            shoppingCartService.updateById(cartServiceOne);
+        } else {
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartService.save(shoppingCart);
+
+            cartServiceOne = shoppingCart;
+        }
+        return R.success(cartServiceOne);
+    }
+
+
     @GetMapping("/{id}")
     public R<ShoppingCart> getById(@PathVariable Long id) {
         log.info("ShoppingCart - getById:id==>{}", id);
@@ -37,27 +109,5 @@ public class ShoppingCartController {
     }
 
 
-    @PostMapping
-    public R<String> save(@RequestBody ShoppingCart shoppingCart) {
-        log.info("ShoppingCart - save:shoppingCart)=>{}", shoppingCart);
-        boolean save = shoppingCartService.save(shoppingCart);
-        return R.flag(save);
-    }
-
-
-    @PutMapping
-    public R<String> update(@RequestBody ShoppingCart shoppingCart) {
-        log.info("ShoppingCart - update:shoppingCart)=>{}", shoppingCart);
-        boolean flag = shoppingCartService.updateById(shoppingCart);
-        return R.flag(flag);
-    }
-
-
-    @DeleteMapping
-    public R<String> delete(@RequestParam("idList") List<Long> ids) {
-        log.info("ShoppingCart - delete:idList)=>{}", ids);
-        boolean flag = shoppingCartService.removeByIds(ids);
-        return R.flag(flag);
-    }
 }
 

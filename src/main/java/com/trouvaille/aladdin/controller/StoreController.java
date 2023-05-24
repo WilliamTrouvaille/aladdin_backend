3d8a99c3e9a0132236704de78c8ab7e310af63a5
william_trouvaille <70807372+WilliamTrouvaille@users.noreply.github.com>
2023-05-24 13:36:30 +0800

    -

--- src/main/java/com/trouvaille/aladdin/controller/StoreController.java
+++ src/main/java/com/trouvaille/aladdin/controller/StoreController.java
@@ -24,11 +24,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequestMapping ("/store")
-/**
- * @deprecated
- * 已弃用!!!!!!!!
- */
-@Deprecated
+
 public class StoreController {
     
     @Autowired
@@ -42,35 +38,35 @@ public class StoreController {
         LambdaQueryWrapper<Store> lqw = new LambdaQueryWrapper<>();
         lqw.like(name != null , Store::getName , name);
         lqw.orderByDesc(Store::getUpdateTime);
-        storeService.page(pageInfo , lqw);
+        this.storeService.page(pageInfo , lqw);
         return R.success(pageInfo);
     }
     
     @GetMapping ("/list")
     public R<List<Store>> list () {
         log.info("获取全部门店信息");
-        List<Store> stores = storeService.list(null);
+        List<Store> stores = this.storeService.list(null);
         return R.success(stores);
     }
     
     @PostMapping ("/save")
     public R<String> save (@RequestBody Store store) {
         log.info("新增门店信息==>{}" , store);
-        boolean save = storeService.save(store);
+        boolean save = this.storeService.save(store);
         return R.flag(save , "新增门店成功!" , "新增门店失败,请重试!");
     }
     
     @PutMapping ("/update")
     public R<String> update (@RequestBody Store store) {
         log.info("更改门店信息==>{}" , store);
-        boolean flag = storeService.updateById(store);
+        boolean flag = this.storeService.updateById(store);
         return R.flag(flag , "更改门店成功!" , "更改门店失败,请重试!");
     }
     
     @GetMapping ("/{id}")
     public R<Store> getById (@PathVariable Long id) {
         log.info("根据id查询门店信息==>{}" , id);
-        Store store = storeService.getById(id);
+        Store store = this.storeService.getById(id);
         return R.success(store);
     }
 }
--- src/main/java/com/trouvaille/aladdin/entity/Store.java
+++ src/main/java/com/trouvaille/aladdin/entity/Store.java
@@ -1,8 +1,6 @@
 package com.trouvaille.aladdin.entity;
 
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -38,19 +36,15 @@ public class Store implements Serializable {
      */
     private String address;
     
-    @TableField (fill = FieldFill.INSERT)
     private LocalDateTime createTime;
     
     
-    @TableField (fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
     
     
-    @TableField (fill = FieldFill.INSERT)
     private Long createUser;
     
     
-    @TableField (fill = FieldFill.INSERT_UPDATE)
     private Long updateUser;
     
     
